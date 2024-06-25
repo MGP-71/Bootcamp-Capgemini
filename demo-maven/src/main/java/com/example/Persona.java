667f75f8094fa248e088872fff3dffedc9ba9437
MGP-71 <miguelgayolpertierra@gmail.com>
2024-06-25 09:52:56 +0200

    Reheacho

--- demo-maven/src/main/java/com/example/Persona.java
+++ demo-maven/src/main/java/com/example/Persona.java
@@ -1,61 +1,56 @@
 package com.example;
 
-
 import java.util.Optional;
 
 public class Persona {
 	private int id;
 	private String nombre;
-	private String apellido;
-	
-	public Persona(int id, String nombre, String apellido) {
-		super();
-		setNombre(nombre);
-		setApellido(apellido);
-		setId(id);
-	}
+	private String apellidos;
 	
 	public Persona(int id, String nombre) {
-		super();
+		this.id = id;
 		setNombre(nombre);
-		setId(id);
 	}
-
+	
+	public Persona(int id, String nombre, String apellidos) {
+		this.id = id;
+		setNombre(nombre);
+		setApellidos(apellidos);
+	}
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
-	public void setNombre(String nombre) {
-		if (nombre == null) throw new IllegalArgumentException("Falta nombre");
-		this.nombre = nombre;
+	public void setNombre(String valor) {
+		if(valor == null || "".equals(valor.trim()))
+			throw new IllegalArgumentException("Falta el nombre");
+		this.nombre = valor;
 	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		if (apellido == null) throw new IllegalArgumentException("Falta apellido");
-		this.apellido = apellido;
-	}
-	
-	public void clearApellidos() {
-		this.apellido = null;
-	}
-	
-	public boolean hasApellidos() {
-		return apellido != null;
-	}
-	
+//	public boolean hasApellidos() {
+//		return apellidos != null;
+//	}
+//	public String getApellidos() {
+//		if(apellidos == null) 
+//			throw new NullPointerException("No tengo apellidos");
+//		return apellidos;
+//	}
 	public Optional<String> getApellidos() {
-		return Optional.ofNullable(apellido);
+		return Optional.ofNullable(apellidos);
 	}
+	public void setApellidos(String valor) {
+		if(valor == null)
+			throw new IllegalArgumentException("Falta los apellidos");
+		this.apellidos = valor;
+	}
+	public void clearApellidos() {
+		this.apellidos = null;
+	}
+	
+	
+	
 }
