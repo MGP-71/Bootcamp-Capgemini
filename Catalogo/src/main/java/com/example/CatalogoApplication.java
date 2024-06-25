7539229b42f6113e16ed498aa54ec6d29b6afd23
MGP-71 <miguelgayolpertierra@gmail.com>
2024-06-25 13:47:10 +0200

    Paso de clases

--- Catalogo/src/main/java/com/example/CatalogoApplication.java
+++ Catalogo/src/main/java/com/example/CatalogoApplication.java
@@ -1,8 +1,12 @@
-package com.example.demo;
+package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.domains.contracts.services.ActorService;
+import com.example.domains.entities.models.ActorDTO;
 
 @SpringBootApplication
 public class CatalogoApplication implements CommandLineRunner {
@@ -11,10 +15,13 @@ public class CatalogoApplication implements CommandLineRunner {
 		SpringApplication.run(CatalogoApplication.class, args);
 	}
 
+	@Autowired
+	ActorService srv;
+
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-
+		System.err.println("Aplicación arrancada...");
+		srv.getByProjection(ActorDTO.class).forEach(System.out::println);
 	}
 
 }
