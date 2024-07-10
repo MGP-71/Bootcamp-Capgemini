a3184d19ba62e511e9ab5a39e1a9f0a70d03034a
MGP-71 <miguelgayolpertierra@gmail.com>
2024-07-10 14:37:42 +0200

    botones

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecurityModule } from './security';
 import { LoggerService, MyCoreModule } from '@my/core';
+import { CommonModule } from '@angular/common';
+import { NotificationComponent } from './main';
+import { DemosComponent } from './demos/demos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecurityModule, MyCoreModule],
+  imports: [CommonModule, RouterOutlet, NotificationComponent, DemosComponent, SecurityModule, MyCoreModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
