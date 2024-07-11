53748225ad77b616013f48f55b0df93fe00e633d
MGP-71 <miguelgayolpertierra@gmail.com>
2024-07-11 09:36:40 +0200

    home

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -3,16 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { LoggerService } from '@my/core';
 import { NotificationComponent, NotificationModalComponent } from './main';
 import { DemosComponent } from './ejemplos';
+import { NotificationService, NotificationType } from './common-services';
+import { HomeComponent } from "./main/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NotificationComponent, NotificationModalComponent, DemosComponent ],
+  imports: [RouterOutlet, NotificationComponent, NotificationModalComponent, DemosComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'World';
 
   // constructor(log: LoggerService) {
   //   log.error('Es un error')
@@ -20,4 +21,10 @@ export class AppComponent {
   //   log.info('Es un info')
   //   log.log('Es un log')
   // }
+//  constructor(private notify: NotificationService) { }
+
+//  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+//  ngOnInit(): void {
+//   this.notify.add('Aplicación arrancada', NotificationType.info)
+//  }
 }
