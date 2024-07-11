1264d2d4717a250c32d7c1eeb4cf88a89f5bc56f
Miguel <149084230+MGP-71@users.noreply.github.com>
2024-07-11 14:56:29 +0200

    cadenas.pipe

--- curso/src/app/ejemplos/demos/demos.component.spec.ts
+++ curso/src/app/ejemplos/demos/demos.component.spec.ts
@@ -1,23 +1,79 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Unsubscribable } from 'rxjs';
+import { NotificationService, NotificationType } from 'src/app/common-services';
 
-import { DemosComponent } from './demos.component';
+@Component({
+  selector: 'app-demos',
+  standalone: true,
+  imports: [CommonModule, FormsModule ],
+  templateUrl: './demos.component.html',
+  styleUrl: './demos.component.css'
+})
+export class DemosComponent implements OnInit, OnDestroy  {
+  private nombre: string = 'mundo'
+  fecha='2024-07-11'
+  fontSize = 24
+  listado = [
+    { id: 1, nombre: 'Madrid'},
+    { id: 2, nombre: 'BARCELONA'},
+    { id: 3, nombre: 'oviedo'},
+    { id: 4, nombre: 'ciudad Real'},
+  ]
+  idProvincia = 2
 
-describe('DemosComponent', () => {
-  let component: DemosComponent;
-  let fixture: ComponentFixture<DemosComponent>;
+  resultado?: string
+  visible = true
+  estetica = { importante: true, error: false, urgente: true }
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DemosComponent]
-    })
-    .compileComponents();
+  constructor(public vm: NotificationService) { }
 
-    fixture = TestBed.createComponent(DemosComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  public get Nombre(): string { return this.nombre }
+  public set Nombre(value: string) {
+    if(this.nombre === value) return
+    this.nombre = value
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  public saluda(): void {
+    this.resultado = `Hola ${this.Nombre}`
+  }
+
+  public despide(): void {
+    this.resultado = `Adios ${this.Nombre}`
+  }
+
+  public di(algo: string): void {
+    this.resultado = `Dice ${algo}`
+  }
+
+  cambia() {
+    this.visible = !this.visible
+    this.estetica.error = !this.estetica.error
+    this.estetica.importante = !this.estetica.importante
+  }
+
+  public calcula(a: number, b: number): number { return a + b }
+
+  public add(provincia: string) {
+    const id = this.listado[this.listado.length - 1].id + 1
+    this.listado.push({id, nombre: provincia})
+    this.idProvincia = id
+  }
+
+  private suscriptor?: Unsubscribable;
+
+  ngOnInit(): void {
+    this.suscriptor = this.vm.Notificacion.subscribe(n => {
+      // if (n.Type !== NotificationType.error) { return; }
+      // window.alert(`Suscripción: ${n.Message}`);
+      // this.vm.remove(this.vm.Listado.length - 1);
+    });
+  }
+  ngOnDestroy(): void {
+    if (this.suscriptor) {
+      this.suscriptor.unsubscribe();
+    }
+  }
+
+}
