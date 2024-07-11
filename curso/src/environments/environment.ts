714e4eed305ad9b51d372508d84030c5418c1f38
MGP-71 <miguelgayolpertierra@gmail.com>
2024-07-11 08:31:11 +0200

    11/07

--- curso/src/environments/environment.ts
+++ curso/src/environments/environment.ts
@@ -1,5 +1,4 @@
-
 export const environment = {
   production: true,
-  ERROR_LEVEL: 1
+  ERROR_LEVEL: 1,
 };
--- curso/src/lib/my-core/services/logger.service.ts
+++ curso/src/lib/my-core/services/logger.service.ts
@@ -1,16 +1,16 @@
 import { Inject, Injectable, InjectionToken, Optional } from '@angular/core';
 
 export const ERROR_LEVEL = new InjectionToken<string>('ERROR_LEVEL')
-
 @Injectable(
   // { providedIn: 'root' }
 )
 export class LoggerService {
-  private nivel: number = 99
+  private readonly nivel: number
 
   constructor(@Optional() @Inject(ERROR_LEVEL) nivel?: number) {
-    // if (nivel || nivel == 0)
+    // if(nivel || nivel === 0) {
     //   this.nivel = nivel
+    // }
     this.nivel = nivel ?? 99
   }
 
@@ -24,10 +24,11 @@ export class LoggerService {
   }
   public info(message: string): void {
     if (this.nivel > 2)
-      if (console.info)
+      if (console.info) {
         console.info(message)
-      else
+      } else {
         console.log(message)
+      }
   }
   public log(message: string): void {
     if (this.nivel > 3)
