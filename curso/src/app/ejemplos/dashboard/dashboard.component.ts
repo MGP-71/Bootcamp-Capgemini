188d83c3bf259be2b4f05c32e03cc879ecf94ed8
Miguel <149084230+MGP-71@users.noreply.github.com>
2024-07-12 09:42:05 +0200

    dashboard componnets

--- curso/src/app/ejemplos/dashboard/dashboard.component.ts
+++ curso/src/app/ejemplos/dashboard/dashboard.component.ts
@@ -2,21 +2,24 @@ import { Component } from '@angular/core';
 import { HomeComponent } from 'src/app/main';
 import { DemosComponent } from '../demos/demos.component';
 import GraficoSvgComponent from 'src/lib/my-core/components/grafico-svg/grafico-svg.component';
+import { NotificationComponent } from "../../main/notification/notification.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NotificationComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
   menu = [
-    { texto: 'inicio', icon: '', componente: HomeComponent },
-    { texto: 'demos', icon: '', componente: DemosComponent },
-    { texto: 'gráfico', icon: '', componente: GraficoSvgComponent }
+    { texto: 'inicio', icono: '', componente: HomeComponent },
+    { texto: 'demos', icono: '', componente: DemosComponent},
+    { texto: 'gráfico', icono: '', componente: GraficoSvgComponent },
   ]
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   actual: any = this.menu[0].componente
+
   seleccionar(indice: number) {
     this.actual = this.menu[indice].componente
   }
