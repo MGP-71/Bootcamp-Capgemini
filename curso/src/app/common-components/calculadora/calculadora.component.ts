1997a5073b1999acb5bfbe00265967f9f69847b5
Miguel <149084230+MGP-71@users.noreply.github.com>
2024-07-12 12:42:43 +0200

    calculadora final

--- curso/src/app/common-components/calculadora/calculadora.component.ts
+++ curso/src/app/common-components/calculadora/calculadora.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { evaluate } from 'mathjs';
 
 @Component({
-  selector: 'app-calculadora',
   standalone: true,
-  imports: [],
+  selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrl: './calculadora.component.css'
+  styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+  displayValue: string = '';
 
+  appendToDisplay(value: string) {
+    this.displayValue += value;
+  }
+
+  clearDisplay() {
+    this.displayValue = '';
+  }
+
+  calculateResult() {
+    try {
+      this.displayValue = evaluate(this.displayValue);
+    } catch (e) {
+      this.displayValue = 'Error';
+    }
+  }
 }
